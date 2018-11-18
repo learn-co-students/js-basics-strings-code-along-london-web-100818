83c429966b4f8e6d50ec7b59e1be654c0d68c5de
congo-quinine <congo.quinine@gmail.com>
2018-11-18 21:46:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+let currentUser = 'User';
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+const a = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+let shortGreeting = `Welcome, ${a}!`;
